67a5c26d2a6085deea6a61e2f6616b2cfa63aa74
wojtek <wojtek.developer@gmail.com>
2015-11-04 11:08:55 +0100

    add map of morse code

--- app/src/main/java/pl/wojtek/morseflashlight/ui/activity/MainActivity.kt
+++ app/src/main/java/pl/wojtek/morseflashlight/ui/activity/MainActivity.kt
@@ -2,17 +2,16 @@ package pl.wojtek.morseflashlight.ui.activity
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.view.View
 import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.activity_main.navigationView;
-import kotlinx.android.synthetic.activity_main.drawer;
+import android.view.View
+import kotlinx.android.synthetic.activity_main.drawer
+import kotlinx.android.synthetic.activity_main.navigationView
 import pl.wojtek.morseflashlight.R
 import pl.wojtek.morseflashlight.extension.find
 import pl.wojtek.morseflashlight.ui.fragments.MainFunctionalityFragment
